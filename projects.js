a7734569bab9e14ec6b435396072fd25e24e6a33
scritch1sm <scritch@scritch.net>
2025-02-02 15:56:13 -0800

    adding initial files

---
+++ projects.js
@@ -0,0 +1,92 @@
+const projects = [
+    { 
+        title: "Netflix Games", 
+        image: "https://picsum.photos/400/400?random=1",
+        placeholderText: "Led design for Netflix's cloud gaming platform"
+    },
+    {
+        title: "Horizon Worlds",
+        image: "https://picsum.photos/400/400?random=2",
+        placeholderText: "Meta's social VR platform design leadership"
+    },
+    {
+        title: "Star Wars: Uprising",
+        image: "https://picsum.photos/400/400?random=3",
+        placeholderText: "Mobile RPG set in the Star Wars universe"
+    },
+    {
+        title: "Gunpowder",
+        image: "https://picsum.photos/400/400?random=4",
+        placeholderText: "Interactive narrative game for HBO"
+    },
+    {
+        title: "First Wonder",
+        image: "https://picsum.photos/400/400?random=5",
+        placeholderText: "Award-winning mobile adventure game"
+    },
+    {
+        title: "My Amazing Story",
+        image: "https://picsum.photos/400/400?random=6", 
+        placeholderText: "Interactive storytelling platform"
+    },
+    {
+        title: "Escape from Monkey Island",
+        image: "https://picsum.photos/400/400?random=7",
+        placeholderText: "Legendary point-and-click adventure sequel"
+    },
+    {
+        title: "Star Wars: Republic Commando",
+        image: "https://picsum.photos/400/400?random=8",
+        placeholderText: "Tactical FPS set in the Star Wars universe"
+    },
+    {
+        title: "Infected (PSP)",
+        image: "https://picsum.photos/400/400?random=9",
+        placeholderText: "Zombie shooter for PlayStation Portable"
+    },
+    {
+        title: "Afterburner: Black Falcon",
+        image: "https://picsum.photos/400/400?random=10",
+        placeholderText: "Arcade-style flight combat game"
+    },
+    {
+        title: "Tangled",
+        image: "https://picsum.photos/400/400?random=11",
+        placeholderText: "Disney movie tie-in game development"
+    },
+    {
+        title: "Jackpot Gems",
+        image: "https://picsum.photos/400/400?random=12",
+        placeholderText: "Mobile puzzle game with social features"
+    },
+    {
+        title: "Smarty Pants",
+        image: "https://picsum.photos/400/400?random=13",
+        placeholderText: "Trivia game for Wii platform"
+    },
+    {
+        title: "Dead on Delivery",
+        image: "https://picsum.photos/400/400?random=14",
+        placeholderText: "Zombie delivery simulator game"
+    },
+    {
+        title: "Throne Together",
+        image: "https://picsum.photos/400/400?random=15",
+        placeholderText: "Royal strategy mobile game"
+    },
+    {
+        title: "Infected (mobile)", 
+        image: "https://picsum.photos/400/400?random=16",
+        placeholderText: "Mobile version of popular zombie shooter"
+    },
+    {
+        title: "Sam & Max 2",
+        image: "https://picsum.photos/400/400?random=17",
+        placeholderText: "Sequels to cult classic adventure series"
+    },
+    {
+        title: "RTX: Red Rock",
+        image: "https://picsum.photos/400/400?random=18",
+        placeholderText: "Early FPS pioneer for PC gaming"
+    }
+];
